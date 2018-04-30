649f5465e5189337a9f78ba60e8a3880f07c6ddd
Maciej Urbańczyk <urbanczyk.maciej.95@gmail.com>
2018-04-30 14:46:20 +0200

    Pre final version of app

--- src/app/modules/user-profile/services/user-profile.service.ts
+++ src/app/modules/user-profile/services/user-profile.service.ts
@@ -1,7 +1,7 @@
 import { Injectable }     from '@angular/core';
 import { HttpClient }     from '@angular/common/http';
 
-import { Observable }     from 'rxjs/observable';
+import { Observable }     from 'rxjs';
 
 @Injectable()
 export class UserProfileService {
